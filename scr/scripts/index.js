e644c6fbd54a0a505590903d09e3d51a327db0c1
Victor E B Barros <victoremanuel6778@gmail.com>
2025-01-08 18:12:29 -0300

    first-commit

---
+++ scr/scripts/index.js
@@ -0,0 +1,8 @@
+let x = document.querySelector(".x");
+let o = document.querySelector(".o");
+let boxes = document.querySelectorAll(".box");
+let buttons = document.querySelectorAll("#menu-container button");
+let message = document.querySelector("#win-message p");
+let secondPlayer;
+
+// contador de jogadas
