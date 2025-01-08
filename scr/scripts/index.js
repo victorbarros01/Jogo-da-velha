2421c6a62c37e24e7852e736880dcf3bee5a7547
Victor E B Barros <victoremanuel6778@gmail.com>
2025-01-08 20:51:55 -0300

    08/01

--- scr/scripts/index.js
+++ scr/scripts/index.js
@@ -6,3 +6,43 @@ let message = document.querySelector("#win-message p");
 let secondPlayer;
 
 // contador de jogadas
+let player1 = 0;
+let player2 = 0;
+
+for (let i = 0; i < boxes.length; i++) {
+  boxes[i].addEventListener("click", function () {
+    let el = checkEl(player1, player2);
+
+    // Condição para verificar se já foi usado x ou o na caixa.
+    if (this.childNodes.length == 0) {
+      let cloneEL = el.cloneNode(true);
+      this.appendChild(cloneEL);
+
+      // Alterna quem está jogando
+      if (player1 == player2) {
+        player1++;
+      } else {
+        player2++;
+      }
+
+      checkWinCondition();
+    }
+  });
+}
+
+// Função para verificar de quem é a vez.
+function checkEl(player1, player2) {
+  if (player1 == player2) {
+    el = x;
+  } else {
+    el = o;
+  }
+
+  return el;
+}
+
+function checkWinCondition() {}
+
+buttons.forEach((button) => {
+  button.addEventListener("click", function () {});
+});
